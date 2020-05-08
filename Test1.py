578fc32be63c505675b8695475ec459fd33d596a
asifahmed786 <asifahmed1us1@gmail.com>
2020-05-08 18:23:23 +0500

    test python

---
+++ Test1.py
@@ -0,0 +1,6 @@
+print("hello","world")
+listOfCities = ["Karachi","lahore","Islamabad"]
+for b in listOfCities:
+    print(b)
+dic = {"Roll":[2,1,4,5,6], "Name":["Asif","Ahmed","Imran","Junaid","Javed"]}
+print(dic)
