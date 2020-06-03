ee1ce77685de8fdc901bd43d978b42ca12a657d9
asifahmed786 <asifahmed1us1@gmail.com>
2020-06-03 12:47:24 +0500

    exam pract 2

--- listpractice.py
+++ listpractice.py
@@ -1,59 +1,68 @@
-abc = 10;
-print(abc)
-print(type(abc//5))
+# abc = 10;
+# print(abc)
+# print(type(abc//5))
 
 
-fname="Asif"
-lname = "Ahmed"
-print(f"My name is {fname} {lname}")
-print("My name is {}{}".format(fname,lname))
+# fname="Asif"
+# lname = "Ahmed"
+# print(f"My name is {fname} {lname}")
+# print("My name is {}{}".format(fname,lname))
 
-print("asif",end="*")
-print("ahmed",end="*")
-print("imran",end="*")
+# print("asif",end="*")
+# print("ahmed",end="*")
+# print("imran",end="*")
 
-print("asif","ahmed",sep="%")
+# print("asif","ahmed",sep="%")
 
-asif = "asif"
-Asif = "asif"
-print(asif)
-print(Asif)
+# asif = "asif"
+# Asif = "asif"
+# print(asif)
+# print(Asif)
 
-if asif==Asif:
-    print("true")
-else:
-    print("false")
-
-cities = ["Karachi","Islamabad","Rawalpindi","Quetta"]
-print(len(cities))
-if "Karachi" in cities:
-    print("true")
-else:
-    print("false")
-
-if "Karach" in cities or "Islamabad" in cities:
-    print("true")
+# if asif==Asif:
+#     print("true")
 # else:
-    print("false")
+#     print("false")
 
-cities.append("hello")
-print(cities)
+# cities = ["Karachi","Islamabad","Rawalpindi","Quetta"]
+# print(len(cities))
+# if "Karachi" in cities:
+#     print("true")
+# else:
+#     print("false")
 
-print(cities.count("Karachi"))
-cities.insert(0,"new hello")
-print(cities)
-cities.sort(reverse=False)
-print(cities)
-cities.pop()
+# if "Karach" in cities or "Islamabad" in cities:
+#     print("true")
+# else:
+    # print("false")
+
+# cities.append("hello")
+# print(cities)
+
+# print(cities.count("Karachi"))
+# cities.insert(0,"new hello")
+# print(cities)
+# cities.sort(reverse=False)
+# print(cities)
+# cities.pop()
 
 
-print(cities)
+# print(cities)
 
-print(cities[-5:-1])
-
-
-abc = [2,4,5,6]
-abc = abc + [10]
-print(abc)
+# print(cities[-5:-1])
 
 
+# abc = [2,4,5,6]
+# abc = abc + [10]
+# print(abc)
+
+# input("please enter your name")
+
+
+dict1 = dict()
+dict1.update({"hello":"new"})
+print(dict1)
+dict1.update({"hello1":"new1"})
+print(dict1)
+dict1["newone"] = "hello"
+print(dict1)
