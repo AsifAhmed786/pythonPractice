8ceb28ef385f7bde5d3d51761ebcc43ccc02e32f
asifahmed786 <asifahmed1us1@gmail.com>
2020-06-19 22:50:24 +0500

    Practice functions

---
+++ func1.py
@@ -0,0 +1,18 @@
+def func1(x,y):
+    return x*y
+
+print(func1(5,6))
+
+def func2(x,y=5):
+    return x*y
+
+print(func2(5))
+
+def func3(x,y,*d):
+    return x,y,d
+
+print(type(func3(4,5,6,7,8)))
+abc = func3(4,5,6,7,8)
+for a in abc:
+    print(a)
+print(abc[0])
