fec393ef1ab6375c2a697200568cdeffab45d882
asifahmed786 <asifahmed1us1@gmail.com>
2020-06-30 15:35:47 +0500

    practice

---
+++ testP1.py
@@ -0,0 +1,25 @@
+print("hello","hello2",sep="-")
+print("hello3")
+
+
+# name = input("what is your name")
+# print(name)
+
+
+'''''Multiline comments'''''
+#single line comment
+
+# global:
+abc = 0
+
+while(abc<12):
+    abc = abc+1
+    print(abc)
+    
+str1 = "hello","hello2"
+print(len(str1))
+
+for a in str1:
+    print(a)
+
+print(type(str1))
